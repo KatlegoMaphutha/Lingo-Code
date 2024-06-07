cd79719ecb7b1ede2c486709dc257f127175d1ef
Katlego Maphutha <157880685+KatlegoMaphutha@users.noreply.github.com>
2024-06-07 15:28:11 +0200

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,7 @@ import type { Config } from "drizzle-kit";
 export default {
   schema: "./db/schema.ts",
   out: "./drizzle",
-  driver: "dl",
+  driver: "pg",
   dbCredentials: {
     connectionString: process.env.DATABASE_URL!,
   },
